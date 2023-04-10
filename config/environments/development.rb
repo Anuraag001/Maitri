9474ac5d0bdf1de388e09fea07ff716ac2113414
Anish Kumbhar <anish.kumbhar04@gmail.com>
2023-04-10 19:39:39 +0530

    song added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
+ config.hosts << "4572-2409-408c-be91-8b4a-a482-d8e6-aff3-290f.ngrok-free.app"
   # Do not eager load code on boot.
   config.eager_load = false
 
