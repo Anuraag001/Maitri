1f037c98c1c3d1a3103a8beaf5d76b61b9028939
Anuraag001 <anuraagbv1@gmail.com>
2023-05-11 19:34:41 +0530

    Created page Videochat and forums

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
   get 'home/chat'
+  get 'home/videochat'
+  get 'home/forums'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
